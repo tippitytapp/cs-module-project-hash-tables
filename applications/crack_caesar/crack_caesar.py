67271120e0af3f4ceb8296486a3464f6f8b3c352
Marc Tapp <marc.tapp@gmail.com>
2020-07-09 08:46:59 -0400

    finished ceasr

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -9,6 +9,7 @@ ciphertext.close()
 chars_to_ignore = ['\n', ' ', '!', '"', "'", '(', ')', ',', '-', '.', '1', ":", ';', '?', 'â', '”', '€' ]
 count_cypher = {}
 # decode_table={}
+# gt count of individual letters
 def letter_count(s):
     for c in s:
         if c in chars_to_ignore:
@@ -19,13 +20,14 @@ def letter_count(s):
             count_cypher[c] = 1
     return count_cypher
 
+# get total number of letters
 def add_all_letters(count):
     total_letters = 0
     for i in count:
         amount = i[1]
         total_letters += amount
     return total_letters
-    
+# calculate the frequency
 def get_frequency(letters):
     freq = {}
     total = add_all_letters(letters)
@@ -33,20 +35,20 @@ def get_frequency(letters):
         amount = i[1]
         freq[i[0]] = round(amount/total * 100, 2)
     return freq
-
+# compare the 2 freq tables
+decode_table = {}
 def compare_freq(nor, cyp):
-    decode_table = {}
-    norm = list(nor.items())
-    cyph = list(cyp)
-    norm.sort(key= lambda e: e[1], reverse=True)
-    cyph.sort(key= lambda e: e[1], reverse=True)
-    for i in norm:
-        for j in cyph:
-            if norm[i[1]] == cyph[j[1]]:
-                decode_table[i[0]] = cyph[j[0]]
-            return decode_table
+        for i in nor:
+            for j in cyp:
+                if i[1] == j[1]:
+                    decode_table[i[0]] = j[0]
+                    # decode_table[i] = cyp[j[0]]
+        return decode_table
 
-
+def add_ignore_to_decoded(arr):
+    for i, v in enumerate(arr):
+        decode_table[v] = v
+    return decode_table
 
 # letters.sort()
 letter_count(text)
@@ -56,12 +58,30 @@ letters.sort()
 # totals = add_all_letters(letters)
 freq = get_frequency(letters)
 
-decoded = compare_freq(normal_freq, freq)
-print('decoded', decoded)
+# decoded = compare_freq(normal_freq, freq)
+# print('decoded', decoded)
 cypher_freq = list(freq.items())
 cypher_freq.sort(key= lambda e: e[1], reverse=True)
+normal = list(normal_freq.items())
 # print('sorted letters', letters)
 # print('add all letters total', add_all_letters(letters))
 # print('freq', get_frequency(letters))
-# print(freq)
+# print(cypher_freq)
+# print(normal)
 # print(normal_freq)
+encoded = compare_freq(normal, cypher_freq)
+add_ignore_to_decoded(chars_to_ignore)
+decoded={'L': 'J'}
+for k,v in encoded.items():
+    decoded[v] = k
+
+# decode the text
+# print(decoded)
+def decode(s):
+    r=""
+    # print(decoded)
+    for c in s:
+        r += decoded[c]
+    return r
+
+print(decode(text))
